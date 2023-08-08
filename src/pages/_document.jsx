bfb46cb1824ece040c2c970b4050c68f6e0aa850
Yogesh <yogeshkhadka34@gmail.com>
2023-08-08 17:52:02 +0545

    fix: build error

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -17,7 +17,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@500;@600&display=swap"
           rel="stylesheet"
-        ></link> */}
+        ></link>
       </Head>
       <body>
         <Main />
